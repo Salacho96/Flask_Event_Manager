3d003fbdb6372cb716fd7aa788fe65059437efa6
dbook-1629 <dbook-1629@outlook.com>
2025-08-03 23:07:26 -0500

    update 3

--- todor/__init__.py
+++ todor/__init__.py
@@ -12,10 +12,12 @@ from todor.api.v1.event_routes import bp as event_bp
 from todor.api.v1.attendee_routes import bp as attendee_bp
 from todor.api.v1.session_routes import bp as session_bp
 from todor.api.v1.user_routes import bp as user_bp
+from flask_cors import CORS
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
     app.config.from_object(config_class)
 
     app.config.from_mapping(
